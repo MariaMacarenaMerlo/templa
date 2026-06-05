982463268ed582a0eed20e10ef4bb4804e412e8d
macarena merlo <macarena.merlo@gmail.com>
2026-06-05 19:47:00 -0300

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Templa",
+        short_name: "Templa",
+        description: "Un refugio digital para volver a la calma",
+        theme_color: "#253241",
+        background_color: "#F3F0EC",
+
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
